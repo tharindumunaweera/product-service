32ebfc4b458db485b0c2633d16c7d33463e394fe
tharindumunaweera <tharindumunaweera94@gmail.com>
2023-09-26 21:50:58 +0530

    implemeted product get and post end points

--- src/main/java/com/tharindumunaweera/productservice/service/ProductService.java
+++ src/main/java/com/tharindumunaweera/productservice/service/ProductService.java
@@ -1,16 +1,38 @@
 package com.tharindumunaweera.productservice.service;
 
 import com.tharindumunaweera.productservice.dto.ProductRequest;
+import com.tharindumunaweera.productservice.dto.ProductResponse;
 import com.tharindumunaweera.productservice.model.Product;
 import com.tharindumunaweera.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
 
     private final ProductRepository productRepository;
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(product -> mapToProductResponse(product)).collect(Collectors.toList());
+
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
 
     public void createProduct(ProductRequest productRequest) {
         Product product = Product.builder()
@@ -18,5 +40,9 @@ public class ProductService {
                 .description(productRequest.getDescription())
                 .price(productRequest.getPrice())
                 .build();
+
+        productRepository.save(product);
+
+        log.info("product {} is saved", product.getId());
     }
 }
